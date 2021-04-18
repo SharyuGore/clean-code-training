f82c64ac299c807d01f0b6638dc875da33b6b59a
sharyu <sharyugore.17@gmail.com>
2021-04-18 14:09:00 +0530

    Excercise 2 completed

--- src/test/java/com/a/introduction/gildedrose/GildedRoseCBackstagePassesTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseCBackstagePassesTest.java
@@ -5,41 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class GildedRoseCBackstagePassesTest {
-	
+
+	private static final int QUALITY_INCREASES = 3;
+	private static final int ITEM_QUALITY = 3;
+	private static final int EXPIRED_SELLIN = 15;
+
 	@Test
-	public void testUpdateQualityBackstagePasses1() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
+	public void sellinReducesBy1_QualityIncreasesBy1() {
+
+		GildedRose app = getItemAndQuality(EXPIRED_SELLIN, ITEM_QUALITY);
 		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(14, app.items[0].sellIn);
-		assertEquals(4, app.items[0].quality);
+
+		Item expected = new Item("Backstage passes to a TAFKAL80ETC concert", EXPIRED_SELLIN - 1, ITEM_QUALITY + 1);
+
+		assertItem(app.items[0], expected);
 	}
 
 	@Test
 	public void testUpdateQualityBackstagePasses2() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
+		GildedRose app = getItemAndQuality(EXPIRED_SELLIN, ITEM_QUALITY);
 		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(6, app.items[0].sellIn);
-		assertEquals(5, app.items[0].quality);
+
+		Item expected = new Item("Backstage passes to a TAFKAL80ETC concert", EXPIRED_SELLIN - 1, ITEM_QUALITY + 1);
+
+		assertItem(app.items[0], expected);
 	}
 
 	@Test
 	public void testUpdateQualityBackstagePasses3() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 3);
+		GildedRose app = getItemAndQuality(EXPIRED_SELLIN, ITEM_QUALITY);
+		app.updateQuality();
+
+		Item expected = new Item("Backstage passes to a TAFKAL80ETC concert", EXPIRED_SELLIN - 1,
+				QUALITY_INCREASES + 1);
+
+		assertItem(app.items[0], expected);
+	}
+
+	private void assertItem(Item item, Item expected) {
+		assertEquals(expected.name, item.name);
+		assertEquals(expected.sellIn, item.sellIn);
+		assertEquals(expected.quality, item.quality);
+	}
+
+	private GildedRose getItemAndQuality(int unexpiredSellin, int unexpiredQuality) {
+		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", unexpiredSellin, unexpiredQuality);
 		Item[] items = new Item[] { item };
 		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(3, app.items[0].sellIn);
-		assertEquals(6, app.items[0].quality);
+		return app;
 	}
 
 }
